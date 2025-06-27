0d7b45f08cd31ce4a86f8eef3150a7ec43081258
richadali <richadyaminali@gmail.com>
2025-06-27 16:45:48 +0530

    Fix asset paths in app layout to ensure proper formatting and consistency in HTML structure for improved asset management.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,11 +12,11 @@
     <title>{{ config('app.name', 'Prasthan Yatnam') }}</title>
 
     <!-- Favicons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
-    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset(" apple-touch-icon.png") }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset(" favicon-32x32.png") }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset(" favicon-16x16.png") }}">
+    <link rel="manifest" href="{{ asset(" site.webmanifest") }}">
+    <link rel="shortcut icon" href="{{ asset(" favicon.ico") }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
