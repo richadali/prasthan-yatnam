001d17fbd8b8ede1fdba393ab2f38ca3694bdebd
richadali <richadyaminali@gmail.com>
2025-09-17 22:27:30 +0530

    Improve spacing for Upcoming Discourses button in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -464,9 +464,10 @@
             @endif
         </div>
     </div>
-        <div class="text-center mt-4">
-            <a href="{{ route('discourses.index') }}#upcoming-discourses" class="btn navy-btn px-4 py-2">Upcoming Discourses</a>
-        </div>
+    <div class="text-center mt-5">
+        <a href="{{ route('discourses.index') }}#upcoming-discourses" class="btn navy-btn px-4 py-2">Upcoming
+            Discourses</a>
+    </div>
     </div>
 </section>
 
